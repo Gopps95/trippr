7826c75f7d4c6fb628961d904e6f2ade44c992c8
gopps95 <shankargopi7@gmail.com>
2024-03-25 21:30:12 +0530

    added locations

---
+++ locations.py
@@ -0,0 +1,15 @@
+LOCATIONS = {
+    'Ernakulam': ['Ernakulam District', 'Ernakulam Town', 'Marine Drive', 'Hill Palace Museum', 'Dutch Cemetery'],
+    'Fort Kochi': ['Fort Kochi', 'Santa Cruz Basilica', 'Dutch Palace', 'Jewish Synagogue', 'Chinese Fishing Nets'],
+    'Mattancherry': ['Mattancherry Palace', 'Dutch Cemetery', 'Spice Market'],
+    'Cherai Beach': ['Cherai Beach', 'Cherai Beach Resort'],
+    'Munnar': ['Munnar', 'Eravikulam National Park', 'Tea Gardens', 'Mattupetty Dam', 'Echo Point'],
+    'Thekkady': ['Periyar National Park', 'Periyar Tiger Trail', 'Spice Gardens', 'Elephant Junction'],
+    'Athirappilly': ['Athirappilly Waterfalls', 'Vazhachal Waterfalls'],
+    'Kumarakom': ['Kumarakom Bird Sanctuary', 'Vembanad Lake', 'Backwaters', 'Pathiramanal Island'],
+    'Marari Beach': ['Marari Beach', 'Marari Beach Resort'],
+    'Alappuzha': ['Alappuzha Beach', 'Backwaters', 'Houseboats', 'Nehru Trophy Boat Race'],
+    'Thrissur': ['Vadakkunnathan Temple', 'Thrissur Pooram', 'Archaeological Museum'],
+    'Kottayam': ['Kumaranalloor', 'Kumarakom Bird Sanctuary', 'Vembanad Lake'],
+    'Idukki': ['Idukki Wildlife Sanctuary', 'Meesapulimala', 'Kalvari Mount', 'Idukki Dam']
+}
